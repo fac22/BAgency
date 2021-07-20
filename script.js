30e776d79896464fa205d6d700d7e1b8f497d32e
aaadriana <adriana@stanciu.uk>
2021-07-20 17:04:11 +0300

    single quotes

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 /* eslint-disable strict */
 
-"use strict";
+'use strict';
 
-const btnFindOutMore = document.querySelector(".btn--find-out-more");
-const sectionAbout = document.querySelector("#chapter-about");
-const navbar = document.querySelector(".navbar");
+const btnFindOutMore = document.querySelector('.btn--find-out-more');
+const sectionAbout = document.querySelector('#chapter-about');
+const navbar = document.querySelector('.navbar');
 
 // Button FIND OUT MORE
-btnFindOutMore.addEventListener("click", () => {
-  sectionAbout.scrollIntoView({ behavior: "smooth" });
+btnFindOutMore.addEventListener('click', () => {
+  sectionAbout.scrollIntoView({ behavior: 'smooth' });
 });
 
 // Page navigation
-document.querySelector(".nav__links").addEventListener("click", (e) => {
+document.querySelector('.nav__links').addEventListener('click', (e) => {
   e.preventDefault();
-  if (e.target.classList.contains("nav__link")) {
-    const id = e.target.getAttribute("href");
-    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
 
 // Navbar fade animation
 const handleHover = (e) => {
-  if (e.target.classList.contains("nav__link")) {
+  if (e.target.classList.contains('nav__link')) {
     const link = e.target;
-    const siblings = link.closest(".navbar").querySelectorAll(".nav__link");
-    const logo = link.closest(".navbar").querySelector(".nav__logo");
+    const siblings = link.closest('.navbar').querySelectorAll('.nav__link');
+    const logo = link.closest('.navbar').querySelector('.nav__logo');
 
     siblings.forEach((el) => {
       const element = el;
@@ -34,17 +34,17 @@ const handleHover = (e) => {
     logo.style.opacity = this;
   }
 };
-navbar.addEventListener("mouseover", handleHover.bind(0.5));
-navbar.addEventListener("mouseout", handleHover.bind(1));
+navbar.addEventListener('mouseover', handleHover.bind(0.5));
+navbar.addEventListener('mouseout', handleHover.bind(1));
 
 // Sticky navigation
 const stickyNavigation = (entries) => {
   const [entry] = entries;
-  if (!entry.isIntersecting) navbar.classList.add("sticky");
-  else navbar.classList.remove("sticky");
+  if (!entry.isIntersecting) navbar.classList.add('sticky');
+  else navbar.classList.remove('sticky');
 };
 
-const header = document.querySelector(".header");
+const header = document.querySelector('.header');
 const navHeight = navbar.getBoundingClientRect().height;
 const headerObserver = new IntersectionObserver(stickyNavigation, {
   root: null,
